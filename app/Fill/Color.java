8b8c91de7b31c440a1f79cea9371c984e7b0f583
Henrycke Schenberk - Parallax <Oschenberk@gmail.com>
2023-08-27 14:00:29 -0300

    Update Color.java

--- app/Fill/Color.java
+++ app/Fill/Color.java
@@ -5,11 +5,6 @@ enum Color {
     BLACK("\033[0;30m"),    // BLACK
     WHITE("\033[0;37m"),    // WHITE
     RED("\033[0;31m"),      // RED
-    GREEN("\033[0;32m"),    // GREEN
-    YELLOW("\033[0;33m"),   // YELLOW
-    BLUE("\033[0;34m"),     // BLUE
-    MAGENTA("\033[0;35m"),  // MAGENTA
-    CYAN("\033[0;36m"),     // CYAN
     RESET("\033[0m");
 
     private String code;
